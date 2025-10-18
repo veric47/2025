269b7392052ca635ed32aeea4211fc52c492123b
veric47 <omeveriku@gmail.com>
2025-10-18 21:58:55 +0100

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Projects from "./pages/Projects";
-import Team from "./pages/Team";
-import Certifications from "./pages/Certifications";
-import Contact from "./pages/Contact";
-
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50 text-gray-800">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/certifications" element={<Certifications />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div  style={{ backgroundColor: "red", height: "100vh" }}>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+        Hello from Accentuate 👋
+      </h1>
+    </div>
   );
 }
 
