ef607ff213ce7bd06ca7ede5fda1739aeb2027d9
veric47 <omeveriku@gmail.com>
2025-10-19 09:44:15 +0100

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
