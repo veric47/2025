af5e1209e5d785778f7ee922a8c756d4d2c19ba6
veric47 <omeveriku@gmail.com>
2025-10-19 08:36:46 +0100

    o

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-     <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
-      <Navbar />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
- 
