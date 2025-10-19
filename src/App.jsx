0c4a942024bd0cca5d7f25649925c57b5b95a5c8
veric47 <omeveriku@gmail.com>
2025-10-19 08:35:17 +0100

    J

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div  style={{ backgroundColor: "red", height: "100vh" }}>
-      <h1 style={{ textAlign: "center", marginTop: "50px" }}>
-        Hello from Accentuate 👋
-      </h1>
+     <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
+      <Navbar />
+      <main className="flex-grow">
+        <Home />
+      </main>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+ 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,36 @@ export default function Navbar() {
           Accentuate
         </Link>
         <ul className="flex gap-6">
-          <li><Link to="/" className="hover:text-yellow-400">Home</Link></li>
-          <li><Link to="/about" className="hover:text-yellow-400">About</Link></li>
-          <li><Link to="/services" className="hover:text-yellow-400">Services</Link></li>
-          <li><Link to="/projects" className="hover:text-yellow-400">Projects</Link></li>
-          <li><Link to="/team" className="hover:text-yellow-400">Team</Link></li>
-          <li><Link to="/contact" className="hover:text-yellow-400">Contact</Link></li>
+          <li>
+            <Link to="/" className="hover:text-yellow-400">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-yellow-400">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" className="hover:text-yellow-400">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className="hover:text-yellow-400">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/team" className="hover:text-yellow-400">
+              Team
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-yellow-400">
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
