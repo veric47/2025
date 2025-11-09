45743c37b2dac4de9c0686063fd06df05e40e1ce
veric47 <omeveriku@gmail.com>
2025-11-09 12:51:44 +0100

    k

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
           <li key={item}>
             <Link
               to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-              style={{ color: "black", textDecoration: "none" }}
+              style={{ color: "white", textDecoration: "none" }}
             >
               {item}
             </Link>
