31da1e407cc73b672113f6a17a9ae461590296a5
veric47 <omeveriku@gmail.com>
2025-11-13 21:26:28 +0100

    n

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -11,7 +11,7 @@ export default function Services() {
       title: "Procurement",
       desc: "We supply high-quality materials and equipment across diverse industries, combining global sourcing and efficient logistics to meet client specifications and enhance operational efficiency.",
       icon: "📦",
-      image: "https://unsplash.com/photos/seven-construction-workers-standing-on-white-field-x-ghf9LjrVg"
+      image: "https://images.unsplash.com/photo-1541888946425-d81bb19240f5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       title: "Energy Consulting",
